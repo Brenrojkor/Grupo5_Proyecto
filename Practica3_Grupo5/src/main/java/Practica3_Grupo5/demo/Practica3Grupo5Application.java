e880365d7c7a7810a987750deb522087298d8496
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-02-23 14:26:25 -0600

    Archivo practica 3

---
+++ Practica3_Grupo5/src/main/java/Practica3_Grupo5/demo/Practica3Grupo5Application.java
@@ -0,0 +1,13 @@
+package Practica3_Grupo5.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica3Grupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica3Grupo5Application.class, args);
+	}
+
+}
---
+++ Practica3_Grupo5/src/test/java/Practica3_Grupo5/demo/Practica3Grupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica3_Grupo5.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica3Grupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
