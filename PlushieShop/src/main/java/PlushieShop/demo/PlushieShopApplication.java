8aa267d93cafa1b5fabcff711a9e7c4ae20efd7b
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-02-23 14:06:12 -0600

    Creacion del archivo del proyecto

---
+++ PlushieShop/src/main/java/PlushieShop/demo/PlushieShopApplication.java
@@ -0,0 +1,13 @@
+package PlushieShop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlushieShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlushieShopApplication.class, args);
+	}
+
+}
