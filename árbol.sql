4316a2e63ac998ec1d82fea3c48c85eacecd98e9
Melanivargas24 <mvargas30424@ufide.ac.cr>
2024-02-27 01:53:30 -0600

    Se insertaron los datos a la tabla

--- árbol.sql
+++ árbol.sql
@@ -12,4 +12,13 @@ nombre  VARCHAR(50),
 tipo_flor VARCHAR(50),
 dureza VARCHAR(25),
 PRIMARY KEY(id_arbol)
-) ENGINE = InnoDB
+) ENGINE = InnoDB;
+
+INSERT INTO práctica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
+('1', 'https://tienda.grupomundoverde.mx/wp-content/uploads/2020/04/Manzano.jpg', 'Manzano', 'Hermafroditas', 'Media');
+INSERT INTO práctica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
+('2', 'https://m.media-amazon.com/images/I/71trld1SaFS._AC_SL1100_.jpg', 'Cerezo', 'Hermafroditas', 'Media');
+INSERT INTO práctica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
+('3', 'https://i.ebayimg.com/images/g/YcsAAOSwGjZkLzqS/s-l1600.jpg', 'Pino', 'Diclinas', 'Variable');
+INSERT INTO práctica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
+('4', 'https://i.ebayimg.com/images/g/VhYAAOSworRk8SCr/s-l1600.jpg', 'Cedro', 'Unisexual', 'Alta');
