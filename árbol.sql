344f0c9201c813372e0a4c24145decaa1f88fe58
Melanivargas24 <mvargas30424@ufide.ac.cr>
2024-03-06 21:29:06 -0600

    Se eliminó el proyecto antiguo

--- árbol.sql
+++ árbol.sql
@@ -1,10 +1,10 @@
-drop schema if exists práctica;
+drop schema if exists practica;
 drop user if exists usuario_practica;
-CREATE SCHEMA práctica;
+CREATE SCHEMA practica;
 create user 'usuario_practica'identified by 'la_Clave';
-grant all privileges on práctica.* to 'usuario_practica'@'%';
+grant all privileges on practica.* to 'usuario_practica'@'%';
 flush privileges;
-USE práctica;
+USE practica;
 CREATE TABLE arbol(
 id_arbol INT NOT NULL AUTO_INCREMENT,
 ruta_imagen VARCHAR(1024),
@@ -14,11 +14,11 @@ dureza VARCHAR(25),
 PRIMARY KEY(id_arbol)
 ) ENGINE = InnoDB;
 
-INSERT INTO práctica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
+INSERT INTO practica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
 ('1', 'https://tienda.grupomundoverde.mx/wp-content/uploads/2020/04/Manzano.jpg', 'Manzano', 'Hermafroditas', 'Media');
-INSERT INTO práctica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
+INSERT INTO practica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
 ('2', 'https://m.media-amazon.com/images/I/71trld1SaFS._AC_SL1100_.jpg', 'Cerezo', 'Hermafroditas', 'Media');
-INSERT INTO práctica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
+INSERT INTO practica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
 ('3', 'https://i.ebayimg.com/images/g/YcsAAOSwGjZkLzqS/s-l1600.jpg', 'Pino', 'Diclinas', 'Variable');
-INSERT INTO práctica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
+INSERT INTO practica.arbol (`id_arbol`, `ruta_imagen`, `nombre`, `tipo_flor`, `dureza`) VALUES 
 ('4', 'https://i.ebayimg.com/images/g/VhYAAOSworRk8SCr/s-l1600.jpg', 'Cedro', 'Unisexual', 'Alta');
