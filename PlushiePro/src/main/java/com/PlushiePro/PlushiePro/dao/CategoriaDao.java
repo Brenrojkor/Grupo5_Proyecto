9b3ec8676516d53fe6b114c4297d4f8044085038
SusanHidalgo <shidalgo70985@ufide.ac.cr>
2024-03-28 00:12:39 -0600

    Cambios en controller, dao y service
    This reverts commit 29164978d895da7a861319a57ebcd42fa827fc7c.

--- PlushiePro/src/main/java/com/PlushiePro/PlushiePro/dao/CategoriaDao.java
+++ PlushiePro/src/main/java/com/PlushiePro/PlushiePro/dao/CategoriaDao.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.PlushiePro.PlushiePro.dao;
 
-/**
- *
- * @author User
- */
-public interface CategoriaDao {
+import com.PlushiePro.PlushiePro.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaDao extends JpaRepository <Categoria, Long> {
     
 }
