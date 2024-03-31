0681f29d455ba4dd37f8816924f7d9ca61b3cae8
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-03-30 20:13:32 -0600

    push correcion, vista naruto

--- PlushiePro/src/main/java/com/PlushiePro/PlushiePro/domain/Categoria.java
+++ PlushiePro/src/main/java/com/PlushiePro/PlushiePro/domain/Categoria.java
@@ -12,27 +12,27 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "categoria")
+
 public class Categoria implements Serializable {
-
+    
     private static final long serialVersionUID = 1L;
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_categoria")
+    @Column(name="id_categoria")
     private Long idCategoria;
-    private String descripcion;
-    private String rutaImagen;
+    private String nombre;
     private boolean activo;
-
-    @OneToMany
+    
+     @OneToMany
     @JoinColumn(name = "id_categoria", updatable = false)
     List<Producto> productos;
 
     public Categoria() {
     }
 
-    public Categoria(String descripcion, boolean activo) {
-        this.descripcion = descripcion;
+    public Categoria(String categoria, boolean activo) {
+        this.nombre = categoria;
         this.activo = activo;
     }
 }
