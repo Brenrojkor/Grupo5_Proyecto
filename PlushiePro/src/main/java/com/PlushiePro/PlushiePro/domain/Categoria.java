e4414cc00e3364bbe65985788450d087c42a361f
Cristopher Rodríguez <145072878+trebleprodigy@users.noreply.github.com>
2024-03-30 18:52:12 -0600

    Actualizacion con el firebasestorage

--- PlushiePro/src/main/java/com/PlushiePro/PlushiePro/domain/Categoria.java
+++ PlushiePro/src/main/java/com/PlushiePro/PlushiePro/domain/Categoria.java
@@ -12,27 +12,27 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "categoria")
-
 public class Categoria implements Serializable {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
+    @Column(name = "id_categoria")
     private Long idCategoria;
-    private String nombre;
+    private String descripcion;
+    private String rutaImagen;
     private boolean activo;
-    
-     @OneToMany
+
+    @OneToMany
     @JoinColumn(name = "id_categoria", updatable = false)
     List<Producto> productos;
 
     public Categoria() {
     }
 
-    public Categoria(String categoria, boolean activo) {
-        this.nombre = categoria;
+    public Categoria(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
         this.activo = activo;
     }
 }
---
+++ PlushiePro/src/main/java/com/PlushiePro/PlushiePro/service/FirebaseStorageService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.PlushiePro.PlushiePro.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+    
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+    
+    //El BuketName es el <id_del_proyecto> + appspot.com#
+    final String BucketName = "plushieshop-25cd1.appspot.com";
+    
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "plushieshop";
+    
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "plushieshop-25cd1-firebase-adminsdk-ftnwx-0442f8d411.json";
+    
+}
